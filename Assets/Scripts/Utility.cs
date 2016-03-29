c366da7299c4cf140c83318ee4108ac2e641e4ff
RickyCoDev <riccardo.cortelazzo@hotmail.it>
2016-03-29 21:04:46 +0200

    Added main menu

---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Utility : MonoBehaviour {
+
+    public void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
